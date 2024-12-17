693c90b1365d7c9d0cb08382f1c9884033b78d75
Daegyeom Ha <combbm@gmail.com>
2024-12-17 17:22:01 +0900

    feat: add `@EnableJpaAuditing` annotation

--- src/main/kotlin/me/daegyeo/maru/MaruBackendApplication.kt
+++ src/main/kotlin/me/daegyeo/maru/MaruBackendApplication.kt
@@ -2,8 +2,10 @@ package me.daegyeo.maru
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class MaruBackendApplication
 
 fun main(args: Array<String>) {
