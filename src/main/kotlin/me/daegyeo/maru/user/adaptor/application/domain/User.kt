70ad97fcd75be769f6867763f2df467149f51f78
Daegyeom Ha <combbm@gmail.com>
2024-12-17 19:32:42 +0900

    feat: create User domain

---
+++ src/main/kotlin/me/daegyeo/maru/user/adaptor/application/domain/User.kt
@@ -0,0 +1,12 @@
+package me.daegyeo.maru.user.adaptor.application.domain
+
+import me.daegyeo.maru.shared.constant.Vendor
+
+data class User(
+    val userId: String,
+    val vendor: Vendor,
+    val nickname: String,
+    val deletedAt: String,
+    val createdAt: String,
+    val updatedAt: String,
+)
