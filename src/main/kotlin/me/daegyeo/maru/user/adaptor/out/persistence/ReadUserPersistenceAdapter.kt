868854acaf542a913aea22e51c541024598b5556
Daegyeom Ha <combbm@gmail.com>
2024-12-25 01:59:37 +0900

    fix: return null value instead of exception

--- src/main/kotlin/me/daegyeo/maru/user/adaptor/out/persistence/ReadUserPersistenceAdapter.kt
+++ src/main/kotlin/me/daegyeo/maru/user/adaptor/out/persistence/ReadUserPersistenceAdapter.kt
@@ -5,6 +5,7 @@ import me.daegyeo.maru.user.application.domain.User
 import me.daegyeo.maru.user.application.port.out.ReadUserPort
 import org.springframework.stereotype.Component
 import java.util.UUID
+import kotlin.jvm.optionals.getOrNull
 
 @Component
 class ReadUserPersistenceAdapter(
@@ -12,12 +13,12 @@ class ReadUserPersistenceAdapter(
     private val userMapper: UserMapper,
 ) : ReadUserPort {
     override fun readUser(userId: UUID): User? {
-        val user = userRepository.findById(userId).orElseThrow { RuntimeException("User not found") }
-        return userMapper.toDomain(user)
+        val user = userRepository.findById(userId).getOrNull()
+        return user?.let { userMapper.toDomain(it) }
     }
 
     override fun readUserByEmail(email: String): User? {
-        val user = userRepository.findByEmail(email).orElseThrow { RuntimeException("User not found") }
-        return userMapper.toDomain(user)
+        val user = userRepository.findByEmail(email).getOrNull()
+        return user?.let { userMapper.toDomain(it) }
     }
 }
