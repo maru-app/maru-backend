e5a839735c5b36000299663d98acbb8ecf753e4c
Daegyeom Ha <combbm@gmail.com>
2024-12-29 17:06:05 +0900

    fix: use `ZonedDateTime` in `createdAt` and `updatedAt` instead of `LocalDateTime`

--- src/main/kotlin/me/daegyeo/maru/shared/entity/AuditDateTimeEntity.kt
+++ src/main/kotlin/me/daegyeo/maru/shared/entity/AuditDateTimeEntity.kt
@@ -1,21 +1,26 @@
 package me.daegyeo.maru.shared.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.EntityListeners
-import jakarta.persistence.MappedSuperclass
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedDate
+import jakarta.persistence.*
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.ZonedDateTime
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class AuditDateTimeEntity {
-    @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
-    val createdAt: ZonedDateTime? = null
+    var createdAt: ZonedDateTime? = null
 
-    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
-    val updatedAt: ZonedDateTime? = null
+    var updatedAt: ZonedDateTime? = null
+
+    @PrePersist
+    fun prePersist() {
+        createdAt = ZonedDateTime.now()
+        updatedAt = ZonedDateTime.now()
+    }
+
+    @PreUpdate
+    fun preUpdate() {
+        updatedAt = ZonedDateTime.now()
+    }
 }
