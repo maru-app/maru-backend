91596aeb8b101c59642ec841ff1287810f52d326
Daegyeom Ha <combbm@gmail.com>
2024-12-29 16:41:40 +0900

    feat: generate access token when user data is exists

--- src/main/kotlin/me/daegyeo/maru/auth/application/service/OAuthUserSuccessHandler.kt
+++ src/main/kotlin/me/daegyeo/maru/auth/application/service/OAuthUserSuccessHandler.kt
@@ -1,10 +1,16 @@
 package me.daegyeo.maru.auth.application.service
 
+import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import me.daegyeo.maru.auth.application.domain.AccessTokenPayload
 import me.daegyeo.maru.auth.application.domain.RegisterTokenPayload
 import me.daegyeo.maru.auth.application.port.`in`.GenerateJWTUseCase
 import me.daegyeo.maru.auth.application.port.`in`.OAuthUserSuccessUseCase
+import me.daegyeo.maru.shared.constant.Vendor
+import me.daegyeo.maru.shared.exception.ServiceException
+import me.daegyeo.maru.user.application.error.UserError
+import me.daegyeo.maru.user.application.port.`in`.GetUserUseCase
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
@@ -14,6 +20,7 @@ import org.springframework.stereotype.Component
 @Component
 class OAuthUserSuccessHandler(
     private val generateJWTUseCase: GenerateJWTUseCase,
+    private val getUserUseCase: GetUserUseCase,
 ) : OAuthUserSuccessUseCase {
     @Value("\${oauth.redirect-url}")
     private lateinit var redirectUrl: String
@@ -30,15 +37,32 @@ class OAuthUserSuccessHandler(
             val userAttributes = oAuth2User.attributes
             val email = userAttributes["email"] as String
 
-            val registerToken =
-                generateJWTUseCase.generateRegisterToken(
-                    RegisterTokenPayload(
-                        email = email,
-                        vendor = vendor,
-                    ),
-                )
+            try {
+                val existsUser = getUserUseCase.getUserByEmail(email)
+                val token =
+                    generateJWTUseCase.generateAccessToken(
+                        AccessTokenPayload(
+                            email = existsUser.email,
+                            vendor = existsUser.vendor,
+                        ),
+                    )
 
-            response?.sendRedirect("$redirectUrl?token=$registerToken")
+                response?.addCookie(Cookie("_maruToken", token))
+            } catch (e: Exception) {
+                if (e is ServiceException && e.error == UserError.USER_NOT_FOUND) {
+                    val registerToken =
+                        generateJWTUseCase.generateRegisterToken(
+                            RegisterTokenPayload(
+                                email = email,
+                                vendor = Vendor.valueOf(vendor.uppercase()),
+                            ),
+                        )
+
+                    response?.sendRedirect("$redirectUrl?token=$registerToken")
+                } else {
+                    throw e
+                }
+            }
         }
     }
 }
