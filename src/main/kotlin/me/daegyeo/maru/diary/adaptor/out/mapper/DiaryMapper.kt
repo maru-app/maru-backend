3851ef059a701134ebf6ad2de1364d6f2774923d
Daegyeom Ha <combbm@gmail.com>
2025-01-01 14:02:46 +0900

    feat: implement DiaryMapper

---
+++ src/main/kotlin/me/daegyeo/maru/diary/adaptor/out/mapper/DiaryMapper.kt
@@ -0,0 +1,18 @@
+package me.daegyeo.maru.diary.adaptor.out.mapper
+
+import me.daegyeo.maru.diary.application.domain.Diary
+import me.daegyeo.maru.diary.application.persistence.DiaryEntity
+import org.springframework.stereotype.Component
+
+@Component
+class DiaryMapper {
+    fun toDomain(diaryEntity: DiaryEntity): Diary {
+        return Diary(
+            diaryId = diaryEntity.diaryId!!,
+            userId = diaryEntity.user.userId!!,
+            content = diaryEntity.content,
+            createdAt = diaryEntity.createdAt!!,
+            updatedAt = diaryEntity.updatedAt!!,
+        )
+    }
+}
