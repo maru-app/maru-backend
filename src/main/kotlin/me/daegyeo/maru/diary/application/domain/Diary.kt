2c8d724bc50749bee79feb02800a2f1cf5c643e0
Daegyeom Ha <combbm@gmail.com>
2025-01-01 14:02:24 +0900

    fix: fix typo

--- src/main/kotlin/me/daegyeo/maru/diary/application/domain/Diary.kt
+++ src/main/kotlin/me/daegyeo/maru/diary/application/domain/Diary.kt
@@ -1,10 +1,11 @@
 package me.daegyeo.maru.diary.application.domain
 
 import java.time.ZonedDateTime
+import java.util.UUID
 
 data class Diary(
     val diaryId: Long,
-    val userId: Long,
+    val userId: UUID,
     var content: String,
     val createdAt: ZonedDateTime,
     val updatedAt: ZonedDateTime,
