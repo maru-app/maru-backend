3051da32be1b377e24b23ac0d3669cf181cfbdf5
Daegyeom Ha <combbm@gmail.com>
2025-02-06 21:00:32 +0900

    fix: exclude `content` column in ReadAllDiaryPort

--- src/main/kotlin/me/daegyeo/maru/diary/adaptor/out/persistence/DiaryRepository.kt
+++ src/main/kotlin/me/daegyeo/maru/diary/adaptor/out/persistence/DiaryRepository.kt
@@ -1,9 +1,16 @@
 package me.daegyeo.maru.diary.adaptor.out.persistence
 
+import me.daegyeo.maru.diary.application.domain.Diary
 import me.daegyeo.maru.diary.application.persistence.DiaryEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.UUID
 
 interface DiaryRepository : JpaRepository<DiaryEntity, Long> {
-    fun findByUserId(userId: UUID): List<DiaryEntity>
+    @Query(
+        """SELECT new me.daegyeo.maru.diary.application.domain.Diary(d.diaryId, d.title, '', d.createdAt, d.updatedAt) 
+            FROM DiaryEntity d 
+            WHERE d.userId = :userId""",
+    )
+    fun findByUserIdExcludingContent(userId: UUID): List<Diary>
 }
--- src/main/kotlin/me/daegyeo/maru/diary/adaptor/out/persistence/ReadAllDiaryPersistenceAdapter.kt
+++ src/main/kotlin/me/daegyeo/maru/diary/adaptor/out/persistence/ReadAllDiaryPersistenceAdapter.kt
@@ -1,6 +1,5 @@
 package me.daegyeo.maru.diary.adaptor.out.persistence
 
-import me.daegyeo.maru.diary.adaptor.out.mapper.DiaryMapper
 import me.daegyeo.maru.diary.application.domain.Diary
 import me.daegyeo.maru.diary.application.port.out.ReadAllDiaryPort
 import org.springframework.stereotype.Component
@@ -9,10 +8,9 @@ import java.util.UUID
 @Component
 class ReadAllDiaryPersistenceAdapter(
     private val diaryRepository: DiaryRepository,
-    private val diaryMapper: DiaryMapper,
 ) : ReadAllDiaryPort {
     override fun readAllDiaryByUserId(userId: UUID): List<Diary> {
-        val diaries = diaryRepository.findByUserId(userId)
-        return diaries.map { diaryMapper.toDomain(it) }
+        val diaries = diaryRepository.findByUserIdExcludingContent(userId)
+        return diaries
     }
 }
