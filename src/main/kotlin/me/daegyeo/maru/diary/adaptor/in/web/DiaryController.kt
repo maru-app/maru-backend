1a140512bbb07a04d08bc491fb298873df306d07
Daegyeom Ha <combbm@gmail.com>
2025-02-06 21:01:27 +0900

    feat: implement `GET /diary` api

--- src/main/kotlin/me/daegyeo/maru/diary/adaptor/in/web/DiaryController.kt
+++ src/main/kotlin/me/daegyeo/maru/diary/adaptor/in/web/DiaryController.kt
@@ -4,9 +4,11 @@ import me.daegyeo.maru.auth.application.domain.CustomUserDetails
 import me.daegyeo.maru.diary.adaptor.`in`.web.dto.CreateDiaryDto
 import me.daegyeo.maru.diary.application.domain.Diary
 import me.daegyeo.maru.diary.application.port.`in`.CreateDiaryUseCase
+import me.daegyeo.maru.diary.application.port.`in`.GetAllDiaryUseCase
 import me.daegyeo.maru.diary.application.port.`in`.command.CreateDiaryCommand
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/diary")
 class DiaryController(
     private val createDiaryUseCase: CreateDiaryUseCase,
+    private val getAllDiaryUseCase: GetAllDiaryUseCase,
 ) {
     @PreAuthorize("hasRole('USER')")
     @PostMapping
@@ -30,5 +33,13 @@ class DiaryController(
                 userId = auth.userId,
             ),
         )
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping
+    fun getAllDiary(
+        @AuthenticationPrincipal auth: CustomUserDetails,
+    ): List<Diary> {
+        return getAllDiaryUseCase.getAllDiaryByUserId(auth.userId)
     }
 }
