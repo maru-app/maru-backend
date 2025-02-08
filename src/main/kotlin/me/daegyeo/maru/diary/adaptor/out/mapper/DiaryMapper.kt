2dc5ce9591d3f2d69878e7d585c4ef724aa23c41
Daegyeom Ha <combbm@gmail.com>
2025-02-08 14:50:00 +0900

    feat: return DiaryWithUserId instead of Diary in ReadDiaryPort

--- src/main/kotlin/me/daegyeo/maru/diary/adaptor/out/mapper/DiaryMapper.kt
+++ src/main/kotlin/me/daegyeo/maru/diary/adaptor/out/mapper/DiaryMapper.kt
@@ -1,6 +1,7 @@
 package me.daegyeo.maru.diary.adaptor.out.mapper
 
 import me.daegyeo.maru.diary.application.domain.Diary
+import me.daegyeo.maru.diary.application.domain.DiaryWithUserId
 import me.daegyeo.maru.diary.application.persistence.DiaryEntity
 import org.springframework.stereotype.Component
 
@@ -9,6 +10,17 @@ class DiaryMapper {
     fun toDomain(diaryEntity: DiaryEntity): Diary {
         return Diary(
             diaryId = diaryEntity.diaryId!!,
+            title = diaryEntity.title,
+            content = diaryEntity.content,
+            createdAt = diaryEntity.createdAt!!,
+            updatedAt = diaryEntity.updatedAt!!,
+        )
+    }
+
+    fun toDomainWithUserId(diaryEntity: DiaryEntity): DiaryWithUserId {
+        return DiaryWithUserId(
+            diaryId = diaryEntity.diaryId!!,
+            userId = diaryEntity.userId,
             title = diaryEntity.title,
             content = diaryEntity.content,
             createdAt = diaryEntity.createdAt!!,
