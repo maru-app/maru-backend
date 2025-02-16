4146cc0ab5a5d6fccb1cf996c473cdf31c5c7730
Daegyeom Ha <combbm@gmail.com>
2025-02-17 01:58:23 +0900

    feat: implement file batch to delete pending files

--- src/main/kotlin/me/daegyeo/maru/MaruBackendApplication.kt
+++ src/main/kotlin/me/daegyeo/maru/MaruBackendApplication.kt
@@ -3,9 +3,11 @@ package me.daegyeo.maru
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 class MaruBackendApplication
 
 fun main(args: Array<String>) {
