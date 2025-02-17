9835948487da3344f2c41dc8fedc6086fc55a81d
Daegyeom Ha <combbm@gmail.com>
2025-02-17 16:53:19 +0900

    fix: remove condition of checking file status

--- src/main/kotlin/me/daegyeo/maru/file/adaptor/out/persistence/ReadFilePersistenceAdapter.kt
+++ src/main/kotlin/me/daegyeo/maru/file/adaptor/out/persistence/ReadFilePersistenceAdapter.kt
@@ -3,7 +3,6 @@ package me.daegyeo.maru.file.adaptor.out.persistence
 import me.daegyeo.maru.diary.adaptor.out.mapper.FileMapper
 import me.daegyeo.maru.file.application.domain.File
 import me.daegyeo.maru.file.application.port.out.ReadFilePort
-import me.daegyeo.maru.file.constant.FileStatus
 import org.springframework.stereotype.Component
 import java.util.UUID
 import kotlin.jvm.optionals.getOrNull
@@ -24,11 +23,7 @@ class ReadFilePersistenceAdapter(
     ): File? {
         val file = fileRepository.findByPathAndUserId(path, userId).getOrNull()
         return file?.let {
-            if (it.status != FileStatus.UPLOADED) {
-                null
-            } else {
-                fileMapper.toDomain(it)
-            }
+            fileMapper.toDomain(it)
         }
     }
 }
