3de62d07d961fb205fcc4bccb3382f6ad67780d3
Daegyeom Ha <combbm@gmail.com>
2025-02-18 01:10:19 +0900

    feat: update file status to `USED` when diary created or updated

--- src/main/kotlin/me/daegyeo/maru/diary/application/service/CreateDiaryService.kt
+++ src/main/kotlin/me/daegyeo/maru/diary/application/service/CreateDiaryService.kt
@@ -6,6 +6,8 @@ import me.daegyeo.maru.diary.application.port.`in`.EncryptDiaryUseCase
 import me.daegyeo.maru.diary.application.port.`in`.command.CreateDiaryCommand
 import me.daegyeo.maru.diary.application.port.out.CreateDiaryPort
 import me.daegyeo.maru.diary.application.port.out.dto.CreateDiaryDto
+import me.daegyeo.maru.file.application.port.out.UpdateFilePort
+import me.daegyeo.maru.file.constant.FileStatus
 import me.daegyeo.maru.user.application.port.`in`.GetUserUseCase
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -14,12 +16,20 @@ import org.springframework.transaction.annotation.Transactional
 class CreateDiaryService(
     private val getUserUseCase: GetUserUseCase,
     private val createDiaryPort: CreateDiaryPort,
+    private val updateFilePort: UpdateFilePort,
     private val encryptDiaryUseCase: EncryptDiaryUseCase,
+    private val getImagePathInContentService: GetImagePathInContentService,
 ) :
     CreateDiaryUseCase {
     @Transactional
     override fun createDiary(input: CreateDiaryCommand): Diary {
         val user = getUserUseCase.getUser(input.userId)
+
+        val imagePaths = getImagePathInContentService.getImagePathInContent(input.content)
+        imagePaths.forEach {
+            updateFilePort.updateFileStatus(it, FileStatus.USED)
+        }
+
         val encryptedContent = encryptDiaryUseCase.encryptDiary(input.content)
         val diary =
             createDiaryPort.createDiary(
--- src/main/kotlin/me/daegyeo/maru/diary/application/service/UpdateDiaryService.kt
+++ src/main/kotlin/me/daegyeo/maru/diary/application/service/UpdateDiaryService.kt
@@ -5,6 +5,8 @@ import me.daegyeo.maru.diary.application.port.`in`.GetDiaryUseCase
 import me.daegyeo.maru.diary.application.port.`in`.UpdateDiaryUseCase
 import me.daegyeo.maru.diary.application.port.`in`.command.UpdateDiaryCommand
 import me.daegyeo.maru.diary.application.port.out.UpdateDiaryPort
+import me.daegyeo.maru.file.application.port.out.UpdateFilePort
+import me.daegyeo.maru.file.constant.FileStatus
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
@@ -13,7 +15,9 @@ import java.util.UUID
 class UpdateDiaryService(
     private val updatedDiaryPort: UpdateDiaryPort,
     private val getDiaryUseCase: GetDiaryUseCase,
+    private val updateFilePort: UpdateFilePort,
     private val encryptDiaryUseCase: EncryptDiaryUseCase,
+    private val getImagePathInContentService: GetImagePathInContentService,
 ) : UpdateDiaryUseCase {
     @Transactional
     override fun updateDiary(
@@ -22,6 +26,12 @@ class UpdateDiaryService(
         input: UpdateDiaryCommand,
     ): Boolean {
         val isExistsAndOwnedDiary = getDiaryUseCase.getDiaryByDiaryId(diaryId, userId)
+
+        val imagePaths = getImagePathInContentService.getImagePathInContent(input.content)
+        imagePaths.forEach {
+            updateFilePort.updateFileStatus(it, FileStatus.USED)
+        }
+
         val encryptedContent = encryptDiaryUseCase.encryptDiary(input.content)
         updatedDiaryPort.updateDiary(
             diaryId = isExistsAndOwnedDiary.diaryId,
