e1040d41b3479f96aa9e7e5e08db5ffe8a0898d7
Daegyeom Ha <combbm@gmail.com>
2025-02-23 20:24:55 +0900

    fix: fix not file not being deleted in minio

--- src/main/kotlin/me/daegyeo/maru/diary/application/service/DeleteDiaryService.kt
+++ src/main/kotlin/me/daegyeo/maru/diary/application/service/DeleteDiaryService.kt
@@ -8,6 +8,9 @@ import me.daegyeo.maru.diary.application.port.`in`.GetDiaryUseCase
 import me.daegyeo.maru.diary.application.port.out.DeleteDiaryFilePort
 import me.daegyeo.maru.diary.application.port.out.DeleteDiaryPort
 import me.daegyeo.maru.diary.application.port.out.ReadAllDiaryFilePort
+import me.daegyeo.maru.file.application.port.out.DeleteFilePort
+import me.daegyeo.maru.shared.error.CommonError
+import me.daegyeo.maru.shared.exception.ServiceException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -19,6 +22,7 @@ class DeleteDiaryService(
     private val getDiaryUseCase: GetDiaryUseCase,
     private val readAllDiaryFilePort: ReadAllDiaryFilePort,
     private val deleteDiaryFilePort: DeleteDiaryFilePort,
+    private val deleteFilePort: DeleteFilePort,
     private val minioClient: MinioClient,
 ) :
     DeleteDiaryUseCase {
@@ -32,18 +36,33 @@ class DeleteDiaryService(
     ): Boolean {
         val diary = getDiaryUseCase.getDiaryByDiaryId(diaryId, userId)
         return diary.let {
-            val deletedFiles =
-                readAllDiaryFilePort.readAllDiaryFileByDiaryIdWithFile(it.diaryId)
-                    .map { diaryFile -> DeleteObject(diaryFile.file.path) }
+            val deletedFiles = readAllDiaryFilePort.readAllDiaryFileByDiaryIdWithFile(it.diaryId)
+            val deletedObjects = deletedFiles.map { diaryFile -> DeleteObject(diaryFile.file.path) }
 
-            minioClient.removeObjects(
-                RemoveObjectsArgs.builder()
-                    .bucket(bucket)
-                    .objects(deletedFiles)
-                    .build(),
-            )
+            val deletedObjectResult =
+                minioClient.removeObjects(
+                    RemoveObjectsArgs.builder()
+                        .bucket(bucket)
+                        .objects(deletedObjects)
+                        .build(),
+                )
+            deletedObjectResult.forEach { result ->
+                try {
+                    val error = result.get()
+                    if (error != null) {
+                        // TODO: log error
+                        throw ServiceException(CommonError.INTERNAL_SERVER_ERROR)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    throw ServiceException(CommonError.INTERNAL_SERVER_ERROR)
+                }
+            }
 
             deleteDiaryFilePort.deleteAllByDiaryId(it.diaryId)
+            deletedFiles.forEach {
+                deleteFilePort.deleteFile(it.fileId)
+            }
             deleteDiaryPort.deleteDiary(it.diaryId)
         }
     }
