afd1b35eaba73f8dd2b31fed7780b00eea6d2931
Daegyeom Ha <combbm@gmail.com>
2025-02-27 10:31:45 +0900

    fix: remove checking user data in DeleteUserPersistenceAdapter

--- src/main/kotlin/me/daegyeo/maru/user/adaptor/out/persistence/DeleteUserPersistenceAdapter.kt
+++ src/main/kotlin/me/daegyeo/maru/user/adaptor/out/persistence/DeleteUserPersistenceAdapter.kt
@@ -3,15 +3,11 @@ package me.daegyeo.maru.user.adaptor.out.persistence
 import me.daegyeo.maru.user.application.port.out.DeleteUserPort
 import org.springframework.stereotype.Component
 import java.util.UUID
-import kotlin.jvm.optionals.getOrNull
 
 @Component
 class DeleteUserPersistenceAdapter(private val userRepository: UserRepository) : DeleteUserPort {
     override fun deleteUser(userId: UUID): Boolean {
-        val user = userRepository.findById(userId).getOrNull()
-        return user?.let {
-            userRepository.delete(it)
-            true
-        } ?: false
+        userRepository.deleteById(userId)
+        return true
     }
 }
