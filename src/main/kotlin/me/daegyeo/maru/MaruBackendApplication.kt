233a99abb9309382cbadee534dd11b65ae558cae
Daegyeom Ha <combbm@gmail.com>
2025-03-23 14:39:48 +0900

    feat: add timezone setting

--- src/main/kotlin/me/daegyeo/maru/MaruBackendApplication.kt
+++ src/main/kotlin/me/daegyeo/maru/MaruBackendApplication.kt
@@ -1,10 +1,12 @@
 package me.daegyeo.maru
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.web.config.EnableSpringDataWebSupport
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.TimeZone
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,4 +16,10 @@ class MaruBackendApplication
 
 fun main(args: Array<String>) {
     runApplication<MaruBackendApplication>(*args)
+}
+
+@PostConstruct
+fun started() {
+    val tz = System.getenv("TZ") ?: "Asia/Seoul"
+    TimeZone.setDefault(TimeZone.getTimeZone(tz))
 }
