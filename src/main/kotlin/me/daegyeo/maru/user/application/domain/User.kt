d42f3af961bbd1f664a69e9a4e54eb26e6877240
Daegyeom Ha <combbm@gmail.com>
2025-06-22 19:35:08 +0900

    refactor: implement domain logic and move mapper to User domain from `UserMapper` class

--- src/main/kotlin/me/daegyeo/maru/user/application/domain/User.kt
+++ src/main/kotlin/me/daegyeo/maru/user/application/domain/User.kt
@@ -1,10 +1,11 @@
 package me.daegyeo.maru.user.application.domain
 
 import me.daegyeo.maru.shared.constant.Vendor
+import me.daegyeo.maru.user.application.persistence.UserEntity
 import java.time.ZonedDateTime
 import java.util.UUID
 
-data class User(
+class User(
     val userId: UUID,
     val email: String,
     val vendor: Vendor,
@@ -13,4 +14,45 @@ data class User(
     val createdAt: ZonedDateTime,
     val updatedAt: ZonedDateTime,
     var deletedAt: ZonedDateTime? = null,
-)
+) {
+    fun updateNickname(newNickname: String) {
+        this.nickname = newNickname
+    }
+
+    fun togglePublicRanking() {
+        this.isPublicRanking = !this.isPublicRanking
+    }
+
+    fun markAsDeleted() {
+        this.deletedAt = ZonedDateTime.now()
+    }
+
+    fun toEntity(): UserEntity {
+        return UserEntity(
+            userId = this.userId,
+            email = this.email,
+            vendor = this.vendor,
+            nickname = this.nickname,
+            isPublicRanking = this.isPublicRanking,
+            deletedAt = this.deletedAt,
+        )
+    }
+
+    companion object {
+        /**
+         * userId, createdAt, updatedAt 는 DBMS에 의해 자동으로 생성됩니다.
+         */
+        fun fromEntity(entity: UserEntity): User {
+            return User(
+                userId = entity.userId ?: UUID.randomUUID(),
+                email = entity.email,
+                vendor = entity.vendor,
+                nickname = entity.nickname,
+                isPublicRanking = entity.isPublicRanking,
+                createdAt = entity.createdAt ?: ZonedDateTime.now(),
+                updatedAt = entity.updatedAt ?: ZonedDateTime.now(),
+                deletedAt = entity.deletedAt,
+            )
+        }
+    }
+}
